debfa8666c91d41a7b8dabd07c31074224df18df
Mason Walker <masonewalker@gmail.com>
2023-11-07 21:05:07 -0800

    Added recode syntax.

--- R/data_processing.R
+++ R/data_processing.R
@@ -159,12 +159,12 @@ per_vars_21 <- c('ADJINC', # Income adjustment factor
                  'PINCP',  # Income
                  'RAC1P',  # Recoded race
                  'SOCP',   # Occupation recode
-                 'OCCP'    # Occupation
+                 'OCCP',  # Occupation
+                 'PUMA'
               )
 
 
 # Pulling data for San Diego law professions
-
 sd_law_demos <- data.frame()
 
 for (i in occupation) {
@@ -182,3 +182,77 @@ sd_law_pums <- get_pums(variables = per_vars_21,
 sd_law_demos <- rbind(sd_law_demos, sd_law_pums)
 
 }
+
+
+# Pulling data for all of U.S. law professions
+us_law_demos <- data.frame()
+
+for (i in occupation) {
+  us_law_pums <- get_pums(variables = per_vars_21,
+                          survey = 'acs5',
+                          state = 'all',
+                          year = 2021,
+                          key = Sys.getenv("CENSUSAPI"),
+                          rep_weights = 'person',
+                          show_call = TRUE,
+                          variables_filter = list(OCCP = (i))
+  ) |>
+    filter(!(PUMA %in% sd_pumas_20))
+  
+  us_law_demos <- rbind(us_law_demos, us_law_pums)
+  
+}
+
+
+###--------------------------------------------------
+# Recoding the ACS 5-year file
+# Apply recoding rules to both US and SD data
+
+sd_law_demos_r <- sd_law_demos |>
+  mutate(
+    AGE_R = case_when(AGEP < 18 ~ 1,
+                      AGEP >= 18 & AGEP < 30 ~ 2,
+                      AGEP >= 30 & AGEP < 50 ~ 3,
+                      AGEP >= 50 & AGEP < 65 ~ 4,
+                      AGEP >= 65 ~ 5,
+                      TRUE ~ 6),
+    AGE_R2 = case_when(AGEP < 18 ~ as.numeric(NA),
+                       AGEP >= 18 & AGEP <= 24 ~ 1,
+                       AGEP >= 25 & AGEP <= 34 ~ 2,
+                       AGEP >= 35 & AGEP <= 44 ~ 3,
+                       AGEP >= 45 & AGEP <= 54 ~ 4,
+                       AGEP >= 55 & AGEP <= 64 ~ 5,
+                       AGEP >= 65 & AGEP <= 74 ~ 6,
+                       TRUE ~ 7),
+    RACEETHN_R = case_when(RAC1P == "1" & HISP == "01" ~ "White", # White non-Hispanic
+                           RAC1P == "2" & HISP == "01" ~ "Black", # Black non-Hispanic
+                           RAC1P == "6" & HISP == "01" ~ "Asian", # Asian non-Hispanic
+                           RAC1P %in% c("3", "4", "5", "7", "8", "9") & HISP == "01" ~ "Other non-Hispanic", # Other non-Hispanic
+                           TRUE ~ "Hispanic"),
+    TRAVEL = case_when(JWMNP <= 15 ~ 1, # Travel time of 15min or less to work
+                       JWMNP > 15 & JWMNP <= 30 ~ 2, # Travel time of 15min-30min to work
+                       JWMNP > 30 & JWMNP <= 45 ~ 3, # Travel time of 30min-45min to work
+                       JWMNP > 45 & JWMNP <= 60 ~ 4, # Travel time of 45min-60min to work
+                       JWMNP > 60 & JWMNP <= 90 ~ 5, # Travel time of 60min-90min to work
+                       TRUE ~ 6), # Travel time of 90+min to work
+    WORK_HRS = case_when(WKHP <= 10 ~ 1, # Works 10 or less hrs
+                         WKHP > 10 & WKHP <= 20 ~ 2, # Works 10-20 hrs
+                         WKHP > 20 & WKHP <= 30 ~ 3, # Works 20-30 hrs
+                         WKHP > 30 & WKHP <= 40 ~ 4, # Works 30-40 hrs
+                         WKHP > 40 & WKHP <= 50 ~ 5, # Works 40-50 hrs
+                         WKHP > 50 & WKHP <= 60 ~ 6, # Works 50-60 hrs
+                         TRUE ~ 7), # Works 60+ hrs
+    ))
+  
+    ))))
+
+
+
+###--------------------------------------------------
+# Export the data as csv's for report 
+
+
+write.csv(lawyer_cnt, "lawyers_time_series.csv")
+write.csv("sd_lawyers_demos.csv")
+write.csv()
+
